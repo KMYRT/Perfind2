1fdd99b0ff2ec97d232d1440f9262c8f2b14b295
trebo <guillaumetrebosc@sfr.fr>
2019-02-08 20:37:18 +0100

    fct clic sur bouton

--- app/src/main/java/com/example/ppe1832/perfind/Accueil.java
+++ app/src/main/java/com/example/ppe1832/perfind/Accueil.java
@@ -5,11 +5,19 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import java.awt.*;
+import java.util.*;
+
+
 
 public class Accueil extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button button13, button12, button11, button9, button8, button7, button6, button5, button4, button3, button2;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,8 +45,72 @@ public class Accueil extends AppCompatActivity {
         setContentView(R.layout.activity_accueil);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+
+        button13 = (Button)findViewById(R.id.button13);
+        button13.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
+        button12 = (Button)findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
+        button11 = (Button)findViewById(R.id.button11);
+        button11.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
+        button9 = (Button)findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
+        button8 = (Button)findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
+        button7 = (Button)findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+        
+        button5 = (Button)findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
+        button4 = (Button)findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Faire quelque chose en réponse au clic
+            }
+        });
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
+    
 }
